2b3436da64644376d7221f25529d1fc9a82d28a0
Ken Sodemann <ken@ionic.io>
2022-06-14 11:40:11 -0500

    add the custom icon

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,46 +1,52 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
+    <ion-header>
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Corkscrews</ion-title>
       </ion-toolbar>
     </ion-header>
-    
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
+
+    <ion-content class="ion-padding">
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <ion-icon src="assets/icons/corkscrew.svg"></ion-icon>
+        <ion-icon src="assets/icons/corkscrew.svg" color="danger"></ion-icon>
+        <ion-icon src="assets/icons/corkscrew.svg" color="primary"></ion-icon>
+        <ion-icon src="assets/icons/corkscrew.svg" color="secondary"></ion-icon>
+        <ion-icon src="assets/icons/corkscrew.svg" color="tertiary"></ion-icon>
+        <ion-icon src="assets/icons/corkscrew.svg" color="warning"></ion-icon>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import {
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'HomePage',
+  name: "HomePage",
   components: {
     IonContent,
     IonHeader,
+    IonIcon,
     IonPage,
     IonTitle,
-    IonToolbar
-  }
+    IonToolbar,
+  },
 });
 </script>
 
 <style scoped>
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -48,21 +54,7 @@ export default defineComponent({
   transform: translateY(-50%);
 }
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
+ion-icon {
+  font-size: 50px;
 }
 </style>
